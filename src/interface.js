270adba70d50292284ceb49fc483c64d5bfb5035
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-11 22:13:28 +0100

    working api data extraction

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const list = document.getElementById('story-list');
+
+  storyExtract();
+
+  function storyExtract() {
+    fetch('https://ent9gi2ec4szjte.m.pipedream.net')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+        listMaker(data.message);
+        console.log(data.message);
+      });
+  }
+      
+  function listMaker(array) {
+    let li;
+    console.log(array);
+    array.forEach(element => {
+      li = document.createElement('li') 
+      li.innerText = element.webTitle;
+      list.appendChild(li)
+    })
+  }
 })
